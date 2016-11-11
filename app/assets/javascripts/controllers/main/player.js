07f55af0f1a9252015443cadca5bbfb9d9605f0e
markomedia <mare.pavlovic.93@gmail.com>
2016-11-11 14:02:20 +0000

    Clean up PlayerCtrl

--- app/assets/javascripts/controllers/main/player.js
+++ app/assets/javascripts/controllers/main/player.js
@@ -1,14 +1,4 @@
 /* global angular */
 
 angular.module('mariposa-training')
-  .controller('PlayerCtrl', ['$scope', 'Lecture',
-  function($scope, Lecture) {
-    Lecture.find('5809f5edeabbde0df80e05a1')
-      .then(function (lecture) {
-        console.log(lecture);
-        return lecture.getTest();
-      })
-      .then(function (test) {
-        console.log(test);
-      });
-}]);
+  .controller('PlayerCtrl', ['$scope', function($scope) { }]);
