96ec5d025505277bc7461c1a444adadc43d5a866
markomedia <mare.pavlovic.93@gmail.com>
2016-12-05 15:03:52 +0000

    Removed debugger from courseManagement.js

--- app/assets/javascripts/controllers/management/courseManagement.js
+++ app/assets/javascripts/controllers/management/courseManagement.js
@@ -26,7 +26,6 @@ angular.module('mariposa-training').controller('CourseManagementCtrl', ['$scope'
         $q.all(promises).then(function(responses){
             $scope.errorCount = 0;
             for(var i = 0; i < responses.length; i++){
-                debugger
                 if(!responses[i].data.ok || responses[i].data.message.indexOf("No one") != -1){
                     $scope.errorCount++;
                 }
