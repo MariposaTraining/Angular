f21dc1ffbea752d509bed01f682b1f23023873fc
markomedia <mare.pavlovic.93@gmail.com>
2018-07-06 10:11:46 +0000

    add links to partner sites

--- app/assets/javascripts/controllers/landingPages/landingPages.js
+++ app/assets/javascripts/controllers/landingPages/landingPages.js
@@ -1,6 +1,18 @@
 /* global angular */
 
 angular.module('mariposa-training')
-  .controller('LandingPageCtrl', ['$scope', '$state', function($scope, $state){
+  .controller('LandingPageCtrl', ['$scope', '$state', '$window', function($scope, $state, $window){
+   
+  $scope.goToCALTCM = function(){
+    $window.open("https://www.caltcm.org/");
+  };
+  
+  $scope.goToAdv4Equity = function(){
+    
+  };
+  
+  $scope.goToQCHF = function(){
+    $window.open("https://www.cahf.org/Education-Events/QCHF");
+  };
    
 }]);
