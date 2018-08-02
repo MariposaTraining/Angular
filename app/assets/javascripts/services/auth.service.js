106dff84bf0b47d173e9f9c8c6b9075caf87db55
markomedia <mare.pavlovic.93@gmail.com>
2018-08-02 06:40:01 +0000

    bring back tmpstorage service in auth.service.js

--- app/assets/javascripts/services/auth.service.js
+++ app/assets/javascripts/services/auth.service.js
@@ -1,34 +1,34 @@
 /* global angular */
 
-angular.module('mariposa-training').service('AuthService', ['$http', '$q', 'Session', 'Account', 'TmpStorage', 'USER_ROLES', 
+angular.module('mariposa-training').service('AuthService', ['$http', '$q', 'Session', 'Account', 'TmpStorage', 'USER_ROLES',
   function ($http, $q, Session, Account, TmpStorage, USER_ROLES) {
-  
+
   var getMember = function(){
     return $http.post('Api/getMember', {memberSoid: Session.userId}).then(function success(response){
 
         Session.addMemberObject(response.data.data);
         Account.processMemberObject(response.data)
-          
-        return response;  
+
+        return response;
       }, function error(response){
         return response;
       });
   };
-  
+
   var getNeeds = function(response){
     return $http.post("/Api/GetNeedsInfo", {memberSoid: Session.userId}).then(function(res){
       if(res.data.ok == true)
         Session.setNeedsInfo(res.data.data);
-        
+
       return $http.post("/Api/GetNeedsState", {memberSoid: Session.userId}).then(function(res){
         if(res.data.ok == true)
           Session.setNeedsState(res.data.data);
-          
+
         return response;
       });
     });
   };
-  
+
   this.login = function (credentials) {
     return $http
       .post('Api/getLogin', credentials)
@@ -38,26 +38,26 @@ angular.module('mariposa-training').service('AuthService', ['$http', '$q', 'Sess
           var userRoles = [];
           if(res.data.IsManager) userRoles.push(USER_ROLES.manager);
           if(res.data.IsStudent) userRoles.push(USER_ROLES.student);
-          
+
           Session.create(res.data.MemberSoid, userRoles);
-          
+
           return getMember().then(function(response){
             return getNeeds(response);
-          });     
+          });
         }else{
           return res;
         }
       });
   };
-    
+
   this.roles = ["Administrator", "DON", "DSD", "BOM", "SSD", "AD", "Nurse", "CNA", "Medical Director", "Physician"];
   this.associations = ["CAHF", "QCHF", "CALTCM"];
-  
+
   this.checkEmailAddress = function(email){
     return $http
       .post('Api/GetEmailAddressExists', {emailAddress: email});
   };
-  
+
   this.register = function(info){
       return registerUser(info)
       .then(function success(res) {
@@ -65,9 +65,9 @@ angular.module('mariposa-training').service('AuthService', ['$http', '$q', 'Sess
           var userId = res.data.data;
           var userRoles = ["student"];
           Session.create(userId, userRoles);
-          
-          //TmpStorage.storeAccountData(info, userId);
-          
+
+          TmpStorage.storeAccountData(info, userId);
+
           return getMember().then(function(response){
             return getNeeds(response);
           });
@@ -78,25 +78,25 @@ angular.module('mariposa-training').service('AuthService', ['$http', '$q', 'Sess
         return res;
       });
   };
-  
+
   var registerUser = function(info){
     return $http.post('Api/GetRegister', info);
   };
-  
+
   this.registerByManager = function(info){
     return registerUser(info);
   };
-  
+
   this.changePassword = function(credentials){
     return $http.post('Api/setChangePassword', credentials);
   };
-  
+
   this.passwordRecovery = function(address){
     return $http.post('Api/setPasswordRecovery', {emailAddress: address});
   };
-  
+
   this.setNewsletter = function(subscriber){
     return $http.post("Api/setNewsletter", {firstName: subscriber.firstName, lastName: subscriber.lastName, emailAddress: subscriber.emailAddress});
   };
-  
+
 }]);
